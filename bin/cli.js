cba1fd2d46ec6e1c3146132588da5ccd314b67bc
Jamshedjon Sidiqov <jamshedsidikov34@gmail.com>
2026-09-05 21:43:13 +0200

    Refactor CLI to use MigrateCommand exclusively; remove UtilsCommand and update utility CSS classes with new prefixes

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
-const args = process.argv.slice(2)
+const MigrateCommand = require('../lib/commands/MigrateCommand')
 
-if (args[0] === 'utils') {
-	const UtilsCommand = require('../lib/commands/UtilsCommand')
-	new UtilsCommand().execute()
-} else {
-	const MigrateCommand = require('../lib/commands/MigrateCommand')
-	new MigrateCommand().execute()
-}
+new MigrateCommand().execute()
--- css/utils/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = ['container', 'flex', 'grid']
